c3f6c6215bf44c3d6050eb7dc3f186296b534eb4
Anshul Kishore <anshulkishore310@gmail.com>
2020-10-14 21:01:36 +0530

    Added basic dynamic pages with Templating using handlebars

--- src/app.js
+++ src/app.js
@@ -6,8 +6,28 @@ console.log(__dirname)
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
 
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
 
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Anshul'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
+        name: 'Anshul'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'This is an example help message'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({
