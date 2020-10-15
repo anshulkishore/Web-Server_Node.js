4df8142720d715a65eae2824fad0ad3e99317034
Anshul Kishore <anshulkishore310@gmail.com>
2020-10-15 22:40:56 +0530

    Styled using CSS

--- src/app.js
+++ src/app.js
@@ -48,9 +48,21 @@ app.get('/weather', (req, res) => {
     })
 })
 
-//app.com
-//app.com/help
-//app.com/about
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Anshul',
+        errorMessage: 'Help article not found!'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Anshul',
+        errorMessage: 'Page not found!'
+    })
+})
 
 app.listen(3000, () => {
     console.log('Server is up on port 3000.')
